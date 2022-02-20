c63978b7a6aca4767db5351189eba0a1dc5270ca
Duy <duytang.iu@gmail.com>
2022-02-20 20:03:38 +0800

    First commit with Restaurant files (Styled components)

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
+
+export default function App() {
+  return (
+    <>
+      <RestaurantsScreen />
+      <ExpoStatusBar style="auto" />
+    </>
+  );
+}
