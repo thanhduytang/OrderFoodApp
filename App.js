babb5881b3a9995d16edb42ba45e8269168f784a
Duy <duytang.iu@gmail.com>
2022-02-20 23:12:33 +0800

    Finishing restaurant card

--- App.js
+++ App.js
@@ -2,10 +2,32 @@ import React from "react";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
 
+import {
+  useFonts as useOswald,
+  Oswald_400Regular,
+} from "@expo-google-fonts/oswald";
+import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
+
+import { ThemeProvider } from "styled-components/native";
+import { theme } from "./src/infrastructure/index";
+
 export default function App() {
+  const [oswaldLoaded] = useOswald({
+    Oswald_400Regular,
+  });
+  const [latoLoaded] = useLato({
+    Lato_400Regular,
+  });
+
+  if (!oswaldLoaded || !latoLoaded) {
+    return null;
+  }
+
   return (
     <>
-      <RestaurantsScreen />
+      <ThemeProvider theme={theme}>
+        <RestaurantsScreen />
+      </ThemeProvider>
       <ExpoStatusBar style="auto" />
     </>
   );
--- src/infrastructure/index.js
+++ src/infrastructure/index.js
@@ -1,4 +1,4 @@
-import { colors } from ".colors";
+import { colors } from "./colors";
 import { space, lineHeights } from "./spacing";
 import { sizes } from "./sizes";
 import { fonts, fontWeights, fontSizes } from "./fonts";
