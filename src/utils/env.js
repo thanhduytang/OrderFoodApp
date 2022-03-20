886cd620383af3b4a84ab5036f12340e451db4ca
Duy <duytang.iu@gmail.com>
2022-03-20 21:46:41 +0700

    Add demo app photos

--- src/utils/env.js
+++ src/utils/env.js
@@ -6,5 +6,5 @@ const localHost =
 
 export const isAndroid = Platform.OS === "android";
 export const isDevelopment = process.env.NODE_ENV === "development";
-export const isMock = false;
+export const isMock = true;
 export const host = !isDevelopment || isAndroid ? liveHost : localHost;
